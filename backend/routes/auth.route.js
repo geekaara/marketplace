758f4b26eaac894b980d8887e69148c8653ea18b
Aaradhanah Appalo Eleven <43980950+geekaara@users.noreply.github.com>
2025-01-09 23:13:09 +1000

    added other routes

--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -1,9 +1,10 @@
 import express from "express";
+import { login, logout, signup } from "../controllers/auth.controller.js";
 
 const router = express.Router();
 
-router.get("/signup", (req, res) => {
-  res.send("Signup Page");
-});
+router.get("/signup", signup);
+router.get("/login", login);
+router.get("/logout", logout);
 
 export default router;
