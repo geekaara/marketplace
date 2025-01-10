99961a3907fc4914332df86a288bd72f810be860
Aaradhanah Appalo Eleven <43980950+geekaara@users.noreply.github.com>
2025-01-10 10:44:07 +1000

    Integrate Redis for refresh token storage and add HTTP-only cookies for token management

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,3 +1,4 @@
+import { redis } from "../lib/redis.js";
 import User from "../models/user.model.js";
 import jwt from "jsonwebtoken";
 
@@ -11,6 +12,34 @@ const generateTokens = (id) => {
   return { accessToken, refreshToken };
 };
 
+const verifyToken = (token, secret) => {
+  return jwt.verify(token, secret);
+};
+
+const storeRefreshToken = async (userId, refreshToken) => {
+  await redis.set(
+    `refreshToken:${userId}`,
+    refreshToken,
+    "EX",
+    7 * 24 * 60 * 60
+  ); // 7 days
+};
+
+const setCookies = (res, accessToken, refreshToken) => {
+  res.cookie("accessToken", accessToken, {
+    httpOnly: true, // Prevents client side JS from reading the cookie
+    sameSite: "strict", // Only send cookies in a first
+    secure: process.env.NODE_ENV === "production",
+    maxAge: 15 * 60 * 1000, // 15 minutes
+  });
+  res.cookie("refreshToken", refreshToken, {
+    httpOnly: true,
+    sameSite: "strict",
+    secure: process.env.NODE_ENV === "production",
+    maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
+  });
+};
+
 export const signup = async (req, res) => {
   const { name, email, password } = req.body;
   try {
@@ -19,9 +48,18 @@ export const signup = async (req, res) => {
       return res.status(400).json({ error: "User already exists" });
     }
     const user = await User.create({ name, email, password });
+    //Add refresh token to redis
     const { accessToken, refreshToken } = generateTokens(user._id);
+    await storeRefreshToken(user._id, refreshToken);
+    setCookies(res, accessToken, refreshToken);
+
     return res.status(201).json({
-      user,
+      user: {
+        _id: user._id,
+        name: user.name,
+        email: user.email,
+        role: user.role,
+      },
       message: "User created successfully",
     });
   } catch (error) {
