bb48261bc7e9323a88031e5bd57172466662cb4e
Aaradhanah Appalo Eleven <43980950+geekaara@users.noreply.github.com>
2025-01-27 11:20:25 +1000

    SCRUM-6-analytics-route added analytics route and get profile

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -147,5 +147,10 @@ export const refreshToken = async (req, res) => {
   }
 };
 
-// export const getProfile = async (req, res) => {
-// };
+export const getProfile = async (req, res) => {
+  try {
+    res.json(req.user);
+  } catch (error) {
+    res.status(500).json({ message: "Server Error", error: error.message });
+  }
+};
